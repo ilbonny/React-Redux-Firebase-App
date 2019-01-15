1c11bf56dd12b50bc687d35490e03dad9e9fad6e
marcobonera <marcobonera@MacBook-Pro-di-Marco.local>
2019-01-15 12:30:08 +0100

    add redux

--- marcoplan/src/components/projects/ProjectList.js
+++ marcoplan/src/components/projects/ProjectList.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary';
 
-const ProjectList = () => {
-    return(
-        <div className="project-list section">
-            <ProjectSummary />
-            <ProjectSummary />
-            <ProjectSummary />
-        </div>
-    )
-}
+const ProjectList = ({projects}) => {
 
-export default ProjectList
+    const summaries = projects && projects.map(project => {
+        return (
+          <ProjectSummary project={project} key={project.id} />
+        )
+      })
+
+    return (
+      <div className="project-list section">
+        {summaries }  
+      </div>
+    )
+  }
+  
+  export default ProjectList
